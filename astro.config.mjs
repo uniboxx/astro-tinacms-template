84baebbb97b167c08e3a6b5e80ae46bda023f102
unibox <unibox@duck.com>
2025-05-10 09:10:00 +0200

    edit astro.config to add site_url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import tinaDirective from "./astro-tina-directive/register"
+import tinaDirective from './astro-tina-directive/register';
+import { loadEnv } from 'vite';
+
+const { SITE_URL } = loadEnv(process?.env?.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
-	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+  site: SITE_URL,
+  integrations: [mdx(), sitemap(), react(), tinaDirective()],
 });
